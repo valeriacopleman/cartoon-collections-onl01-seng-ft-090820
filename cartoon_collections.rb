5e7767b54e96d2d09e68ee3e9646faaf2a067d2b
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-14 23:59:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
 def roll_call_dwarves(array)# code an argument here
 
   array.each do |names|
-   names
+  puts names
 end 
-end
+
 
 def summon_captain_planet# code an argument here
   # Your code here
