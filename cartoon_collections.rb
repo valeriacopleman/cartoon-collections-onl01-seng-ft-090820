0bc80eb9ef8d43b396db9648db2ebb026a98fd14
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-15 01:14:59 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+#list of name with number order
+def roll_call_dwarves(array)
+counter = 1 
+  array.each do |names|
+  puts "#{counter}. #{names}"
+  counter +=1 
+end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+#capitalize first letter and add exclamation
+def summon_captain_planet(array)
+  array.collect {|i| i.capitalize + "!"} 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+#returns T if any elements longer than 4 char. or F if all are less than 4 char.
+def long_planeteer_calls(array)
+  array.any? {|x| x.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+    
+  end 
 end
