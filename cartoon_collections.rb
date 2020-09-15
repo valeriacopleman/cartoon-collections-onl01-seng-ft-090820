8122dbc7116adaf06712c6ea84a9c65128de454f
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-15 00:15:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,9 @@ def summon_captain_planet(array)
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|x| x.length > 4}
+
 end
 
 def find_the_cheese# code an argument here
